53cbfa9bf1de11d1aa4f41f0f20f408db03a8d72
Sina Eslami <96436516+sinaEsl@users.noreply.github.com>
2023-06-26 00:38:09 +0330

    Update genetic.py

--- genetic.py
+++ genetic.py
@@ -1,10 +1,13 @@
 import subprocess
 import re
 import random
+import threading
+import json
 
 
 def fitness_function(new_HS):
-    input('input gamecfg')
+    with open('HS.json', 'w') as file:
+        json.dump(new_HS, file)
     result = subprocess.run(['python', 'main.py'], stdout=subprocess.PIPE)
     out_result = result.stdout.decode('utf-8')
     find_blue = re.compile(r'Blue -> -?\d+')
@@ -12,31 +15,43 @@ def fitness_function(new_HS):
     find_number = re.compile(r'-?\d+')
     blue_point = find_blue.findall(out_result)[0]
     yellow_point = find_yellow.findall(out_result)[0]
-    population.append((new_HS, int(find_number.findall(yellow_point)[0]) - int(find_number.findall(blue_point)[0])))
+    population.append(new_HS, int(find_number.findall(yellow_point)[0]) - int(find_number.findall(blue_point)[0]))
 
 
 def mean_crossover(parent1, parent2):
     alpha = random.random()
     value_per_health = alpha * parent1['value per health'] + (1 - alpha) * parent2['value per health']
-    left_and_right = alpha * parent1['left and right'] + (1 - alpha) * parent2['left and right']
-    child = [value_per_health, left_and_right]
+    child = {'value per health': value_per_health}
     return child
 
 
 def mutation(child, mutation_coefficient):
-    mutate_parameter = random.choices(['value per health', 'left and right'])
     mutate_sign = random.choices([1, -1])
-    child[mutate_parameter] += mutation_coefficient * mutate_sign
+    child['value per health'] += mutation_coefficient * mutate_sign
     return child
 
 
-agent1 = {'value per health': 25, 'left and right': 5}
-agent2 = {'value per health': 20, 'left and right': 10}
-agent3 = {'value per health': 30, 'left and right': 2}
-agent4 = {'value per health': 15, 'left and right': 15}
+population = []
 
-population = [(agent1, fitness_function(agent1)), (agent2, fitness_function(agent2)),
-              (agent3, fitness_function(agent3)), (agent4, fitness_function(agent4))]
+agent1 = {'value per health': 25}
+agent2 = {'value per health': 10}
+agent3 = {'value per health': 12}
+agent4 = {'value per health': 15}
+agent5 = {'value per health': 13}
+agent6 = {'value per health': 14}
+
+threads = [threading.Thread(target=fitness_function, args=(agent1,)),
+           threading.Thread(target=fitness_function, args=(agent2,)),
+           threading.Thread(target=fitness_function, args=(agent3,)),
+           threading.Thread(target=fitness_function, args=(agent4,)),
+           threading.Thread(target=fitness_function, args=(agent5,)),
+           threading.Thread(target=fitness_function, args=(agent6,))]
+for thread in threads:
+    input('input gamecfg\n')
+    thread.start()
+    input('confirm\n')
+for thread in threads:
+    thread.join()
 
 num_generations = 5
 
@@ -47,17 +62,29 @@ mutation_possibility = 0.1
 mutation_coefficient = 5
 
 for _ in range(num_generations):
-    for i in range(2):
+    children = []
+    for i in range(3):
         if random.random() < crossover_possibility:
             parent1, parent2 = random.choices([p[0] for p in population], weights=[p[1] for p in population], k=2)
             child = mean_crossover(parent1, parent2)
             if random.random() < mutation_possibility:
                 child = mutation(child, mutation_coefficient)
-                population.append((child, fitness_function(child)))
+            children.append(child)
         else:
             i -= 1
 
+    threads = [threading.Thread(target=fitness_function, args=(children[0],)),
+               threading.Thread(target=fitness_function, args=(children[1],)),
+               threading.Thread(target=fitness_function, args=(children[2],))]
+    for thread in threads:
+        input('input gamecfg\n')
+        thread.start()
+        input('confirm\n')
+    for thread in threads:
+        thread.join()
+
     sorted(population, key=lambda p: p[1], reverse=True)
+    population.pop(0)
     population.pop(0)
     population.pop(0)
 
