79d218a203c8128b190fa1ba32c292cf37f8f045
Sina Eslami <96436516+sinaEsl@users.noreply.github.com>
2023-06-25 20:46:25 +0330

    Update genetic.py

--- genetic.py
+++ genetic.py
@@ -4,8 +4,7 @@ import random
 
 
 def fitness_function(new_HS):
-    print(new_HS)
-    input("change gamecfg")
+    input('input gamecfg')
     result = subprocess.run(['python', 'main.py'], stdout=subprocess.PIPE)
     out_result = result.stdout.decode('utf-8')
     find_blue = re.compile(r'Blue -> -?\d+')
@@ -13,7 +12,7 @@ def fitness_function(new_HS):
     find_number = re.compile(r'-?\d+')
     blue_point = find_blue.findall(out_result)[0]
     yellow_point = find_yellow.findall(out_result)[0]
-    return int(find_number.findall(yellow_point)[0]) - int(find_number.findall(blue_point)[0])
+    population.append((new_HS, int(find_number.findall(yellow_point)[0]) - int(find_number.findall(blue_point)[0])))
 
 
 def mean_crossover(parent1, parent2):
@@ -32,52 +31,37 @@ def mutation(child, mutation_coefficient):
 
 
 agent1 = {'value per health': 25, 'left and right': 5}
-agent2 = {'value per health': 11, 'left and right': 19}
-agent3 = {'value per health': 15, 'left and right': 15}
-agent4 = {'value per health': 5, 'left and right': 25}
-agent5 = {'value per health': 27, 'left and right': 14}
-agent6 = {'value per health': 9, 'left and right': 18}
+agent2 = {'value per health': 20, 'left and right': 10}
+agent3 = {'value per health': 30, 'left and right': 2}
+agent4 = {'value per health': 15, 'left and right': 15}
 
-population = [(agent1, fitness_function(agent1)),
-              (agent2, fitness_function(agent2)),
-              (agent3, fitness_function(agent3)),
-              (agent4, fitness_function(agent4)),
-              (agent5, fitness_function(agent5)),
-              (agent6, fitness_function(agent6))]
+population = [(agent1, fitness_function(agent1)), (agent2, fitness_function(agent2)),
+              (agent3, fitness_function(agent3)), (agent4, fitness_function(agent4))]
 
-num_generations = 10
+num_generations = 5
 
 crossover_type = mean_crossover
 crossover_possibility = 0.9
-
-best_agent = agent1
-max_point = float('-inf')
-for agent, point in population:
-    if point > max_point:
-        best_agent = agent
-        max_point = point
 
 mutation_possibility = 0.1
 mutation_coefficient = 5
 
 for _ in range(num_generations):
-    for i in range(3):
+    for i in range(2):
         if random.random() < crossover_possibility:
             parent1, parent2 = random.choices([p[0] for p in population], weights=[p[1] for p in population], k=2)
             child = mean_crossover(parent1, parent2)
             if random.random() < mutation_possibility:
                 child = mutation(child, mutation_coefficient)
-                point = fitness_function(child)
-                population.append((child, point))
-                if point > max_point:
-                    best_agent = child
-                    max_point = point
+                population.append((child, fitness_function(child)))
         else:
             i -= 1
 
     sorted(population, key=lambda p: p[1], reverse=True)
     population.pop(0)
     population.pop(0)
-    population.pop(0)
 
-    mutation_coefficient -= 0.5
+    mutation_coefficient -= 1
+
+print(population[0])
+print(population)
